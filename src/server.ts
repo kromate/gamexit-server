2434b5a0360774d01df4f580b1fe8696702c613d
kromate <akpananthony33@gmail.com>
2022-04-29 03:04:21 +0100

    feat: omo

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
-import { Server } from 'socket.io'
+// setup server
 
-const io = new Server(9000, {
+const http = require('http')
+const express = require('express')
+const path = require('path')
+const socketio = require('socket.io')
+
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server, {
   cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST']
-  }
+    origin: '*',
+  },
 })
 
 
@@ -18,4 +25,11 @@ io.on('connection', (socket) => {
 })
 
 
-// io.listen(9000)
+// set static folder
+app.use(express.static(path.join(__dirname, 'index.html')))
+
+const PORT = process.env.PORT || 9000
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
