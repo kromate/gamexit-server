e084e87b9a5b01a65c3528c9c7b77634c26496e2
kromate <akpananthony33@gmail.com>
2022-04-30 17:03:45 +0100

    update

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,10 @@ const express = require('express')
 const path = require('path')
 const socketio = require('socket.io')
 
+const socketHandler = require('./utils/MainController')
+const roomHandler = require('./utils/RoomController')
+const gameHandler = require('./utils/GameController')
+
 
 const app = express()
 const server = http.createServer(app)
@@ -18,10 +22,28 @@ const io = socketio(server, {
 
 
 io.on('connection', (socket) => {
-  socket.on('play', (index) => {
-    console.log('server received', index)
-    socket.broadcast.emit('play', index)
+  const handlerSocket = new socketHandler(io, socket)
+  const handlerRooms = new roomHandler(io, socket)
+  const handlerGames = new gameHandler(io, socket)
+
+  handlerSocket.connected()
+
+  socket.on('join_game', (message) => {
+    handlerRooms.joinGame(message)
   })
+
+  socket.on('update_game', (message) => {
+    handlerGames.updateGame(message)
+  })
+
+  socket.on('game_win', (message) => {
+    handlerGames.gameWin(message)
+  })
+
+
+
+
+
 })
 
 
